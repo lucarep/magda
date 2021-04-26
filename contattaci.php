eff6d2b53f53c284b3a4ad6002420a9cb43379e8
chiara1212 <massari.1834921@studenti.uniroma1.it>
2021-04-26 11:31:23 +0200

    alcune modifiche

--- contattaci.php
+++ contattaci.php
@@ -36,6 +36,7 @@ if (isset($_POST['email']) && $_POST['email'] != '') {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <link href="style.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="animate.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </head>
 
@@ -44,7 +45,7 @@ if (isset($_POST['email']) && $_POST['email'] != '') {
   if ($messaggio_inviato) :
   ?>
     <nav class="navbar navbar-expand-lg navbar-dark">
-      <div class="container-fluid">
+      <div class="container-fluid ">
         <a class="navbar-brand" href="index.html">Magda</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -52,8 +53,8 @@ if (isset($_POST['email']) && $_POST['email'] != '') {
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
             <a class="nav-link" href="index.html">Home</a>
-            <a class="nav-link" href="#">Accedi</a>
-            <a class="nav-link" href="#">Registrati</a>
+            <a class="nav-link" href="login.php">Accedi</a>
+            <a class="nav-link" href="registrati.php">Registrati</a>
             <a class="nav-link active" aria-current="page" href="contattaci.php">Contattaci</a>
             <a class="nav-link" href="about_us.html">About us</a>
           </div>
@@ -87,15 +88,15 @@ if (isset($_POST['email']) && $_POST['email'] != '') {
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
             <a class="nav-link" href="index.html">Home</a>
-            <a class="nav-link" href="login.html">Accedi</a>
-            <a class="nav-link" href="registrati.html">Registrati</a>
+            <a class="nav-link" href="login.php">Accedi</a>
+            <a class="nav-link" href="registrati.php">Registrati</a>
             <a class="nav-link active" aria-current="page" href="contattaci.php">Contattaci</a>
             <a class="nav-link" href="about_us.html">About us</a>
           </div>
         </div>
       </div>
     </nav>
-    <div class="container-fluid">
+    <div class="container-fluid animate__animated animate__fadeInDownBig">
       <header id="form_desc">
         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-asterisk" viewBox="0 0 16 16">
           <path d="M8 0a1 1 0 0 1 1 1v5.268l4.562-2.634a1 1 0 1 1 1 1.732L10 8l4.562 2.634a1 1 0 1 1-1 1.732L9 9.732V15a1 1 0 1 1-2 0V9.732l-4.562 2.634a1 1 0 1 1-1-1.732L6 8 1.438 5.366a1 1 0 0 1 1-1.732L7 6.268V1a1 1 0 0 1 1-1z" />
