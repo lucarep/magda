8778db9ac71a227fb20d3cae19a00625d1b1d4d9
chiara1212 <massari.1834921@studenti.uniroma1.it>
2021-05-10 08:52:50 +0200

    aggiunta loading bar a 'le mie piante'

--- lemiepiante.php
+++ lemiepiante.php
@@ -53,11 +53,15 @@ if (isset($_POST["annaffiata"])) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/kursor@0.0.14/dist/kursor.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 
 </head>
 
 <body>
+<div class="loader">
+  </div>
+ <div class="content">
     <style>
         div.card {
             float: left;
@@ -275,6 +279,49 @@ if (isset($_POST["annaffiata"])) {
         });
     </script>
 
+<script>
+/*LOADING*/
+    function loaderSpinner() {
+    $(window).load(function() {
+        var loader = $('.loader');
+var wHeight = $(window).height();
+var wWidth = $(window).width();
+var i = 0;
+
+loader.css({
+    top: wHeight / 2 - 2.5,
+    left: wWidth / 2 - 200
+ })
+      
+  do{
+    loader.animate({
+      width: i
+    },10)
+    i+=3;
+  } while(i <= 400)
+  if(i === 402){
+    loader.animate({
+      left: 0,
+      width: '100%'
+    })
+    loader.animate({
+      top: '0',
+      height: '100vh'
+    })
+  }
+      
+ setTimeout(function(){
+        $('.content').fadeIn("slow");
+        (loader).fadeOut("fast");
+      },2500);
+    });
+
+}
+
+loaderSpinner();
+</script>
+
+
 
 
     <div class="text-center p-3 fixed-bottom" style="background-color:#303926; color: white;">
@@ -283,6 +330,7 @@ if (isset($_POST["annaffiata"])) {
     </div>
 
 
+    </div>
 
 </body>
 
